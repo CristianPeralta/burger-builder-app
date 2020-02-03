32fa75fd1f26167716574825291d5cf6e18bd398
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-03 12:19:16 -0500

    add Layout css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Classes from './Layout.css';
 import Aux from '../../hoc/Aux';
 
 const layout = ( props ) => (
     <Aux>
         <div> Toolbar, SideDrawer, BackDrop </div>
-        <main>
+        <main className={Classes.Content}>
             {props.children}
         </main>
     </Aux>

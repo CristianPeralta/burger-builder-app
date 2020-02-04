d72b7299956eadee2da4a36e5c4bd2015dbf60c2
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-04 18:51:33 -0500

    pass ingredient list

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,15 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return console.log(ingKey);
+        })
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="salad"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bot"/>
         </div>
     );

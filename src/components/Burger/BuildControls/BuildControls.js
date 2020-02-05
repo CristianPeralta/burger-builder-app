6d2b5ed13cddd4b4f51db2155d92327daa9e0f6e
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-05 12:37:13 -0500

    add a BuildControl into BuildControls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import classes from './BuildControls.css';
 
+import BuildControl from './BuildControl/BuildControl';
+
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-
+        <BuildControl label="Salad"/>
     </div>
 );
 

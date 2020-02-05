d35d0a76c4978217d9ad8a91642f0c17a93038f6
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-05 13:42:33 -0500

    disable Less button if ingredient counter is 0

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -54,6 +54,13 @@ class BurgerBuilder extends Component {
     }
 
     render() {
+
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
         return (
             <Aux>
                 <Burger 
@@ -62,6 +69,7 @@ class BurgerBuilder extends Component {
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
                 />
             </Aux>
         )

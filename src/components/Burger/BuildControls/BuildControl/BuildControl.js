ff6a56617dc7d6a816999c3ca298f2f767b1b150
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-05 12:30:55 -0500

    apply BuildControl styles

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
+import classes from './BuildControl.css';
+
 const buildControl = (props) => (
-    <div>
-        <div>
-            
-        </div>
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
     </div>
 );
 

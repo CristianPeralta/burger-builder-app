bd4db4515845d7ddb10b3c6bdec9ecdb1fb678a0
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-06 17:26:29 -0500

    add modal styles

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import classes from './Modal.css';
+
 const modal = (props) => {
     return (
-        <div>
+        <div className={classes.Modal}>
             {props.children}
         </div>
     )

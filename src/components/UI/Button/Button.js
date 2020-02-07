12f9311da4392f45ad1c8eee88983007c9cdd611
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-07 15:30:06 -0500

    apply Button styles

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,10 @@
 import React from 'rect';
 
+import classes from './Button.css';
+
 const button = (props) => (
     <button
+        className={[classes.Button, classes[props.btnType]].join(' ')}
         onClick={props.clicked}
         >
             {props.children}

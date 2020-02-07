8f7a762b315143d5bff9434959374267beabbf06
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-07 16:02:46 -0500

    add BurgerLogo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import BurgerLogo from '../../assets/images/burger-logo.png';
+
 const logo = (props) => (
     <div>
-        
+        <img src={BurgerLogo} alt="Burger App"></img>
     </div>
 );
 

4344eb10cae78522fd011a176c64e7f9ee2ec423
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-09 15:44:50 -0500

    aply SideDrawer styles

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => (
-    <div>
-
+    <div className={classes.SideDrawer}>
+        <Logo />
+        <nav>
+            <NavigationItems />
+        </nav>
     </div>
 );
 

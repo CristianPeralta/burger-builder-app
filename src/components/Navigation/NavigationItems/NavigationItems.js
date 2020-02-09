d9003c1225be2322aa135c146ab4f38d091505ba
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-09 15:27:37 -0500

    add navigation props

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/">Burger Builder</NavigationItem>
-        <NavigationItem link="/">Chackout</NavigationItem>
+        <NavigationItem link="/"active={true} >Burger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
     </ul>
 );
 

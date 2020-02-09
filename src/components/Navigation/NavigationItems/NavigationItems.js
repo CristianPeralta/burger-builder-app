bd199fa333803d88a7e8c2c932cbb7942ab75b48
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-09 15:18:40 -0500

    add Navigation Items into Toolbar component

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem></NavigationItem>
+        <NavigationItem link="/">Burger Builder</NavigationItem>
+        <NavigationItem link="/">Chackout</NavigationItem>
     </ul>
 );
 

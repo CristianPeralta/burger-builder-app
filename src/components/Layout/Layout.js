94322609870971a2718893bebb55fe305db1b545
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-10 16:22:53 -0500

    convert layout to Class component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Classes from './Layout.css';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer />
-        <div> Toolbar, SideDrawer, BackDrop </div>
-        <main className={Classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    render () {
+        return (
+            <Aux>
+                <Toolbar/>
+                <SideDrawer />
+                <div> Toolbar, SideDrawer, BackDrop </div>
+                <main className={Classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
 
-export default layout;
+export default Layout;

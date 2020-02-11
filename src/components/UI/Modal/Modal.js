3e094b27c62d42a607b77f273d045baf4489bf3c
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-11 18:40:37 -0500

    add shouldComponentUpdate into Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,6 +5,10 @@ import Aux from '../../../hoc/Auxw';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
 class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
     componentWillUpdate() {
         console.log('[Modal] WillUpdate');
     }

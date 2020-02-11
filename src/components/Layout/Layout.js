98ac47e308b1201b2159a01f4fa71517225428b3
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-10 21:17:31 -0500

    add sideDrawerToggleHandler

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -12,6 +12,12 @@ class Layout extends Component {
     sideDrawerClosedHandler = () => {
         this.setState({ showSideDrawer: false });
     }
+    sideDrawerToggleHandler =() => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
     render () {
         return (
             <Aux>

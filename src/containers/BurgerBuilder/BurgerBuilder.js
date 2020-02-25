1e5916783ae6ab853b2ef3dd58d0dee9395bde46
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-25 09:23:25 -0500

    post order to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import axios from '../../axios-orders';
 import Aux from '../../hoc/Auxw/Auxw';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
@@ -77,7 +78,23 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('Continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "John Doe",
+                address: {
+                    street: "street 1",
+                    zipCode: "20184",
+                    country: "United States"
+                },
+                email: "johndoe@test.com"
+            },
+            deliveryMethod: "fastest"
+        };
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {

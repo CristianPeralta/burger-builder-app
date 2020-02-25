0c1c555229dcfdb7815bb94d498c3305e5929fc0
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-25 10:05:56 -0500

    add withErrorHanlder as hoc

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const withErrorHanlder = () => {
+    return (
+        <div>
+            With Error Handler
+        </div>
+    );
+};
+
+export default withErrorHanlder;

8ea8fa0e19f9cfb82cb86d1084bc376b990e5a74
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-25 10:19:33 -0500

    add error state

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,11 +4,16 @@ import Modal from '../../components/UI/Modal/Modal';
 
 const withErrorHandler = (WrapperComponent) => {
     return class extends Component {
+        state = {
+            error: {
+                message: "Something went worng!",
+            }
+        };
         render() {
             return (
                 <Aux>
-                    <Modal show>
-                        Something went worng!
+                    <Modal show={this.state.error}>
+                        {this.state.error ? this.state.error.message : null}
                     </Modal>
                     <WrapperComponent {...this.props} />
                 </Aux>

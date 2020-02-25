bc20c5daa7c85083193dfec23249e0f72e016b03
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-25 09:36:39 -0500

    add spinner styles

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import classes from './Spinner.css';
 
 const spinner = () => {
     return (
-        <div>
-            SPINNER
-        </div>
+        <div className={classes.Loader}>Loading...</div>
     )
 };
 

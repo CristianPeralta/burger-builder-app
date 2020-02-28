b88ed1278011b61352ca838b4c291ba2eb92c9db
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-28 06:47:59 -0500

    add Burger component into CheckoutSummary

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Burger from '../../Burger/Burger';
 
 const CheckoutSummary = (props) => {
     return (
         <div>
-            Checkout Summary
+            <h1>We hope it tastes well!</h1>
+            <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients} />
+            </div>
         </div>
     )
 };

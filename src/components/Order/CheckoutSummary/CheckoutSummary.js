cbcc0edc28084c24025df4a506c56efcc4b8786f
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-28 07:02:33 -0500

    add Checkout container ias Layout chilldren

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,14 +7,14 @@ const CheckoutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
-            <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+            <div style={{width: '100%', height: '300px', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients} />
             </div>
             <Button
                 btnType="Danger"
                 clicked>CANCEL</Button>
             <Button
-                btnType="Successr"
+                btnType="Success"
                 clicked>CONTINUE</Button>
         </div>
     )
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
-    state={
-        salad: 1,
-        meat: 1,
-        cheese: 1,
-        baccon: 1,
+    state = {
+        ingredients: {
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            baccon: 1,
+        }
     }
     render() {
         return (

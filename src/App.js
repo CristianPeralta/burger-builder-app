73f57b3e821391c75c014208d5be8120a0c73765
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-28 07:12:50 -0500

    add swicth router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from  './containers/Checkout/Checkout';
@@ -8,8 +8,10 @@ function App() {
   return (
     <div className="App">
       <Layout>
-        <Route path="/" component={BurgerBuilder} />
-        <Route path="/checkout" component={Checkout} />
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
       </Layout>
     </div>
   );

48db4b31653c5931e03415426ed9b7a5d741c103
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-28 07:51:43 -0500

    add contactData states

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class ContactData extends  Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: '',
+        }
+    }
     render() {
         return (
             <div>

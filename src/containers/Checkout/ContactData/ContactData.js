b6346979d5fb8bfd4b1f52ba1c39c5d344494f4d
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-28 07:55:29 -0500

    add form into contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -12,7 +12,13 @@ class ContactData extends  Component {
     render() {
         return (
             <div>
-                Contact Data
+                <h4>Enter your Contact Data</h4>
+                <form>
+                    <input type="text" name="name" placeholder="Your Name" />
+                    <input type="text" name="email" placeholder="Your Mail" />
+                    <input type="text" name="street" placeholder="Street" />
+                    <input type="text" name="postal" placeholder="Postal Code" />
+                </form>
             </div>
         );
     }

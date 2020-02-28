ab0c8db8ee231de125d3ab69c733fd935693b039
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-28 06:58:19 -0500

    add ingredients CheckoutSummary arg

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
+    state={
+        salad: 1,
+        meat: 1,
+        cheese: 1,
+        baccon: 1,
+    }
     render() {
         return (
             <div>
-                <CheckoutSummary />
+                <CheckoutSummary ingredients={this.state.ingredients} />
             </div>
         );
     }

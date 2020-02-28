5473fe668adfc3bdc03bb25b088299206529cecf
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-28 06:55:20 -0500

    add CheckoutSummary into Checkout container

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
     render() {
         return (
             <div>
-                Checkout
+                <CheckoutSummary />
             </div>
         );
     }

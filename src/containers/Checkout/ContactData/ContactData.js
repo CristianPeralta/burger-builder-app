316ba65db2282b9683af2fbfaf3df278ce13b41b
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-28 07:49:44 -0500

    add ContactData component

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactData extends  Component {
+    render() {
+        return (
+            <div>
+                Contact Data
+            </div>
+        );
+    }
+}
+
+export default ContactData;

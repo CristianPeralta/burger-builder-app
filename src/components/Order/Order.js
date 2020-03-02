35c444163110517d9617f0047f599af0072c0bf8
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-02 08:27:05 -0500

    build ingredients list

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,20 @@ import React from 'react';
 import classes from './Order.css';
 
 const order = (props) => {
-    console.log(props);
+    const ingredients = [];
+    for (const ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            mount: props.ingredients[ingredientName],
+        });
+    }
+
+    const ingredientsOutput = ingredients.map(ig => {
+    return <span key={ig.name}> {ig.name} ({ig.mount}) </span>;
+    });
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Meat (1)</p>
+            <p> Ingredients: {ingredientsOutput} </p>
     <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
         </div>
     );

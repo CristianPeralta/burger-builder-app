00f7733f1fcff33f76caafb3680470df4529cfbd
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-02 07:27:42 -0500

    add Orders container

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+class Orders extends Component {
+    render() {
+        return (
+            <div>
+                Orders container
+            </div>
+        );
+    }
+}
+
+export default Orders;

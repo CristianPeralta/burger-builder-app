5c2ff1f77a4b0ac6e0b2e7e6f30f12c1f99ed1f1
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-02 07:31:56 -0500

    add order css

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import classes from './Order.css';
 
 const order = (props) => {
     return (
-        <div>
-            Order Component
+        <div className={classes.Order}>
+            <p>Ingredients: Meat (1)</p>
+            <p>Price: <strong>USD 4.2</strong></p>
         </div>
     );
 };

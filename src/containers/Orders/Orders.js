217b083d7dfd69f71624fab0db6383681e85acfc
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-02 07:58:55 -0500

    get orders json

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: null
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true });
+        axios.get('/orders.json')
+            .then(response => {
+                console.log(response);
+                this.setState({ loading: false });
+            })
+            .catch(error => {
+                this.setState({ loading: false });
+            });
+    }
+
     render() {
         return (
             <div>
+                {this.state.loading ? <p>LOADING...</p> : null}
                 <Order />
                 <Order />
             </div>

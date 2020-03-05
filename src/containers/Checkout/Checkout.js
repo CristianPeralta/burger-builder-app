3706f7c1a8b25903a7e9fca4317b1b5af97afd8d
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-05 07:33:18 -0500

    replace local ingredients state by redux state

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,23 +5,9 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-    state = {
-        ingredients: null,
-        totalPrice: 0,
-    }
 
     componentWillMount() {
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {};
-        let price = 0;
-        for (const param of query.entries()) {
-            if (param[0] === 'price') {
-                price = param[1];
-            } else {
-                ingredients[param[0]] = +param[1];
-            }
-        }
-        this.setState({ ingredients, totalPrice: price });
+        this.setState({ ingredients: this.props.ings });
     }
 
     checkoutCancelledHandler = () => {
@@ -40,7 +26,7 @@ class Checkout extends Component {
                     checkoutContinued={this.checkoutContinuedHandler} />
                 <Route
                     path={this.props.match.path + '/contact-data'}
-                    render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
+                    render={(props) => (<ContactData ingredients={this.props.ings} {...props} />)} />
             </div>
         );
     }

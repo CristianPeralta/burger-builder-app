c7850b09c17c34d146da482263aae28cf781ff8d
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-05 06:52:40 -0500

    add action type handler

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,7 +6,19 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-
+    switch (action.type) {
+        case ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName] : state.ingredients[action.ingredientName] + 1
+                }
+            }
+    
+        default:
+            break;
+    }
 };
 
 export default reducer;

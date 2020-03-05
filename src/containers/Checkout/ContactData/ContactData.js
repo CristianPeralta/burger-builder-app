fd6e55455316b7dd2a6a117dc4d985fc5eeae081
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-05 07:35:02 -0500

    connect ContactData component with redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Button from './../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
@@ -192,4 +194,11 @@ class ContactData extends  Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.price,
+    };
+};
+
+export default connect(mapStateToProps)(ContactData);

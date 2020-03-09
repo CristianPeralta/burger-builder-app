78d8963a22926ad20145b412acf9c5c3155d8569
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-09 13:00:17 -0500

    add order action creator

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,1 +1,9 @@
-// order.js
+import { PURCHASE_BURGER_SUCCESS } from './actionsTypes';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData,
+    };
+};

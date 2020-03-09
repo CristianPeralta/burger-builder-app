33d6dda45b87accd491ce34d4006e16a41385d3b
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-09 13:02:24 -0500

    add order action creator for fail pucharse burger

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,9 +1,16 @@
-import { PURCHASE_BURGER_SUCCESS } from './actionsTypes';
+import { PURCHASE_BURGER_SUCCESS, PURCHASE_BURGER_FAIL } from './actionsTypes';
 
 export const purchaseBurgerSuccess = (id, orderData) => {
     return {
         type: PURCHASE_BURGER_SUCCESS,
         orderId: id,
         orderData: orderData,
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: PURCHASE_BURGER_FAIL,
+        error: error,
     };
 };

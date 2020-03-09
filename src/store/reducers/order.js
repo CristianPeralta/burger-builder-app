cc441ffd07153bee87a081cbceda9f188f68683a
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-09 13:35:13 -0500

    update orders state

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -8,8 +8,14 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderId,
+            };
             return {
                 ...state,
+                loading: false,
+                orders: state.orders.concat(newOrder),
             };
         case PURCHASE_BURGER_FAIL:
             return {

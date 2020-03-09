57534a204df6cfd7e6989438da5fcc789d275c38
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-09 13:31:37 -0500

    add order reducers

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,1 +1,22 @@
-// order.js
+import { PURCHASE_BURGER_SUCCESS, PURCHASE_BURGER_FAIL  } from '../actions/actionsTypes';
+
+const initialState = {
+    orders: [],
+    loading: false,
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case PURCHASE_BURGER_SUCCESS:
+            return {
+                ...state,
+            };
+        case PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+            };
+        default: return state;
+    }
+};
+
+export default reducer;

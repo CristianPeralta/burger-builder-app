c88f7dbe1d4646f6c35778e905faa670ac01224b
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-11 09:55:18 -0500

    add methods for each action type

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -3,7 +3,9 @@ import {
     AUTH_SUCCESS,
     AUTH_FAIL,
 } from './/../actions/actionsTypes';
-import { initIngredients } from '../actions';
+
+import { updateObject } from  '../utility.js';
+import { act } from 'react-dom/test-utils';
 
 const initialState = {
     token: null,
@@ -12,11 +14,28 @@ const initialState = {
     loading: false,
 };
 
+const authStart = (state) => {
+    return updateObject(state, { loading: true, error: null });
+};
+
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        token: action.idToken,
+        userId: action.userId,
+        error: null,
+        loading: false,
+    });
+};
+
+const authFail = (state, action) => {
+    return updateObject(state, { loading: false, error: action.error });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case AUTH_START: return;
-        case AUTH_SUCCESS: return;
-        case AUTH_FAIL: return;
+        case AUTH_START: return authStart(state, action);
+        case AUTH_SUCCESS: return authSuccess(state, action);
+        case AUTH_FAIL: return authFail(state, action);
         default: return state;
     }
 };

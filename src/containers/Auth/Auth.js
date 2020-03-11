c20fcc4907364cfd0246c541c83ce15f5584b951
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-11 07:40:27 -0500

    add email control

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
 class Auth extends Component {
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email Addrees',
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true,
+                },
+                valid: false,
+                touched: false,
+            },
+        }
+    }
     render() {
         return (
             <div>

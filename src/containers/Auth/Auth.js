66f781ee455d32f921b733ae60d71e7748106036
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-11 08:39:02 -0500

    add submithandler

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -78,6 +78,11 @@ class Auth extends Component {
         this.setState({ controls: updatedControls });
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
+
     render() {
         const formElementsFromArray = [];
         for (const key in this.state.controls) {
@@ -102,7 +107,7 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType='Success'>SUBMIT</Button>
                 </form>

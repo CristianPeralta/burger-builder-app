5d7b52388dc3a7b9406a856f203c41776179de81
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-11 08:00:13 -0500

    add and apply auth styles

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import classes from './Auth.css';
 
 class Auth extends Component {
     state = {
@@ -58,7 +59,7 @@ class Auth extends Component {
         ))
 
         return (
-            <div>
+            <div className={classes.Auth}>
                 <form>
                     {form}
                     <Button btnType='Success'>SUBMIT</Button>

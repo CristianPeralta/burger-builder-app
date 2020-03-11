76954f2eb600017dab2a6fcbc0ed28bcc51cf228
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-11 08:23:24 -0500

    add action creator for new action types

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -3,3 +3,23 @@ import {
     AUTH_SUCCESS,
     AUTH_FAIL,
 } from './actionsTypes';
+
+export const authStart = () => {
+    return {
+        type: AUTH_START,
+    };
+};
+
+export const authSuccess = (authData) => {
+    return {
+        type: AUTH_SUCCESS,
+        authData: authData,
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: AUTH_FAIL,
+        error: error,
+    };
+};

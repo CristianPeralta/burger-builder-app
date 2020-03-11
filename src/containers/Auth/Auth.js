540f5c16732f31c9e344b6cd358d5f3463fbd476
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-11 08:04:18 -0500

    add checkValidity for Auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -36,6 +36,30 @@ class Auth extends Component {
             },
         }
     }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (!rules) return true;
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.isEmail) {
+            const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+        return isValid;
+    }
     render() {
         const formElementsFromArray = [];
         for (const key in this.state.controls) {

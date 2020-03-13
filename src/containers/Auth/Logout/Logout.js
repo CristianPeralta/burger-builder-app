7ea411cea5d95ee1099f4010f0a9beff9e9a999e
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-13 09:27:34 -0500

    redirect to home after mount Logout component

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 import { logout } from '../../../store/actions/index';
 
 class Logout extends Component {
+    componentDidMount() {
+        this.props.onLogout();
+    }
     render() {
-        return (
-            <div>
-                Logout
-            </div>
-        );
+        return <Redirect to="/" />
     }
 }
 

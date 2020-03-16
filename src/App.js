832a469a4598f3bae029752e6936d5b5dfbf5f30
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-16 12:23:34 -0500

    add onTryAutoSignUp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { authCheckState } from './store/actions/index';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from  './containers/Checkout/Checkout';
@@ -26,7 +27,7 @@ function App() {
 
 const mapDispatchToProps = dispatch => {
   return {
-
+    onTryAutoSignUp: () => dispatch(authCheckState()),
   };
 };
 

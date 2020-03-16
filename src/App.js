022f2a027f526c67109fa8244431d05c5920d7f3
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-16 12:21:16 -0500

    connect App with redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from  './containers/Checkout/Checkout';
@@ -23,4 +24,10 @@ function App() {
   );
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);

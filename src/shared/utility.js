ffb3eff12d71ac99dbe071e2672fe58d45641e47
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-17 09:16:44 -0500

    add checkValidity as utility

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,27 @@ export const updateObject = (object, updatedProperties) => {
         ...updatedProperties,
     };
 };
+
+export const checkValidity = (value, rules) => {
+    let isValid = true;
+
+    if (!rules) return true;
+
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.isEmail) {
+        const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+};

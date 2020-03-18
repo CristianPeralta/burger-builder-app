e73ba2e2cbb4468fed51263fcc8b62a966593eda
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-18 15:19:26 -0500

    add test to find buildcontrols

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -3,12 +3,18 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import BurgerBuilder from './BurgerBuilder';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 configure({ adapter: new Adapter() });
 
-describe('<NavigationItems />', () => {
+describe('<BurgerBuilder />', () => {
     let wrapper;
     beforeEach(() => {
         wrapper = shallow(<BurgerBuilder />);
+    });
+
+    it('should render two <BurgerBuilder /> when receiving ingredients', () => {
+        wrapper.setProps({ ings: { salad: 0 } });
+        expect(wrapper.find(BuildControls)).toHaveLength(1);
     });
 });

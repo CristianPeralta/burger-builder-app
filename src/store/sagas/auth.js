a8a7acdcdc347004782e3419ef4a9739be2836ef
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-23 10:33:06 -0500

    add redux saga and logout method for auth sagas

---
+++ src/store/sagas/auth.js
@@ -0,0 +1,12 @@
+import { put } from 'redux-saga/effects';
+
+import { AUTH_LOGOUT } from '../actions/actionsTypes';
+
+function logout(action) {
+    yield localStorage.removeItem('token');
+    yield localStorage.removeItem('expirationDate');
+    yield localStorage.removeItem('userId');
+    yield put({
+        type: AUTH_LOGOUT,
+    });
+}
